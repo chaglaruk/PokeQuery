4088a13e97746f44ad0272b634880e44fd63268e
chaglaruk <chaglaruk@gmail.com>
2026-08-18 01:22:03 +0100

    release: bump Android to 0.7.5 code 25

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.caglar.pokequery"
         minSdk = 24
         targetSdk = 36
-        versionCode = 24
-        versionName = "0.7.4"
+        versionCode = 25
+        versionName = "0.7.5"
     }
 
     val keystorePropertiesFile = rootProject.file("keystore.properties")
