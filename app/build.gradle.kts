f3c6fc37ed3393e68d2a894ad5a2ff1d28f4ddeb
chaglaruk <chaglaruk@gmail.com>
2026-08-18 18:30:10 +0100

    release: bump Android to v0.7.6 code 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.caglar.pokequery"
         minSdk = 24
         targetSdk = 36
-        versionCode = 25
-        versionName = "0.7.5"
+        versionCode = 26
+        versionName = "0.7.6"
     }
 
     val keystorePropertiesFile = rootProject.file("keystore.properties")
